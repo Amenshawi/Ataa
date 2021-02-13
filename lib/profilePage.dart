ca097fd2d11a61576792d0e72c4a02320825db4b
Tareq AlRuwais <r_6are8@outlook.com>
2021-02-13 03:11:08 +0300

    added methods for clothing sizes, not functional yet

--- lib/profilePage.dart
+++ lib/profilePage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:Ataa/appUser.dart';
 import 'package:Ataa/auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:Ataa/database.dart';
 import 'package:page_transition/page_transition.dart';
@@ -16,6 +17,11 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  String shirtSize;
+  String pantsSize;
+  int shoeSize;
+  List<int> shoesSizes = [];
+
   double hieghtSize, widthSize;
   bool visiblePassword = false;
   bool isSwitched;
@@ -44,6 +50,8 @@ class _ProfileState extends State<Profile> {
 
   @override
   Widget build(BuildContext context) {
+    for(int i = 1 ; i < 50 ; i++)
+      shoesSizes.add(i);
     Size size = MediaQuery.of(context).size;
     hieghtSize = size.height;
     widthSize = size.width;
@@ -118,7 +126,7 @@ class _ProfileState extends State<Profile> {
                     Icons.check,
                     false,
                     'Location'), // based on the data from the db we can change the icon from add or change (if there is a location then the display icon would be edit and the oppsite).
-                cardBlur(3, Icons.accessibility, 'Clothing Size', Icons.add,
+                cardBlur(3, Icons.accessibility, 'Clothing Size', Icons.edit,
                     Icons.check, false, "Clothes"), // same as card 3
                 Card(
                   shape: RoundedRectangleBorder(
@@ -260,12 +268,14 @@ class _ProfileState extends State<Profile> {
                   changePassword(context);
                 } else if (!blurBackground[index] ||
                     controllers[index].text == '') {
+                      print('second if');
                   controllers[index].clear();
                   setState(() {
                     readWriteToggole[index] = !readWriteToggole[index];
                     blurBackground[index] = !blurBackground[index];
                   });
                 } else if (index == 0) {
+                  print('third if');
                   try {
                     print(controllers[0].text);
                     user = _auth.changeEmail(controllers[0].text, user);
@@ -277,6 +287,9 @@ class _ProfileState extends State<Profile> {
                     print(error.toString);
                     //show error message
                   }
+                }else if(index == 3){
+                  print('in index 3');
+                  clothingCard(context);
                 }
               },
             ),
@@ -438,5 +451,130 @@ class _ProfileState extends State<Profile> {
                   bottom: MediaQuery.of(context).viewInsets.bottom * 0.5),
               child: SingleChildScrollView(child: passwordSheet()));
         });
+  }
+  clothingCard(context){
+    print('in clothing card');
+    showModalBottomSheet(
+        context: context,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+        ),
+        elevation: 10,
+        isScrollControlled: true,
+        builder: (BuildContext bc) {
+          return Padding(
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom * 0.5),
+              child: SingleChildScrollView(child: clothingSheet())
+              );
+        });
+  }
+ Widget clothingSheet(){
+    print('in clothing sheet');
+    return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+      return Container(
+          height: MediaQuery.of(context).size.height * .4,
+          margin: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Shirts/Tshirts'),
+                        DropdownButton(
+                          value: shirtSize,
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 22,
+                          style: TextStyle(
+                            color: ataaGold,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                            ),
+                            underline: Container(height: 2, color: ataaGreen),
+                            onChanged: (String newValue){
+                              setState((){
+                                shirtSize = newValue;
+                              });
+                            },
+                            items: <String>['xS','S','M','L','XL','XXL','XXXL']
+                                    .map<DropdownMenuItem<String>>((String value){
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(), 
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15,),
+                    Row(
+                      children: [
+                        Text('Pants Size'),
+                        DropdownButton(
+                          value: pantsSize,
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 22,
+                          style: TextStyle(
+                            color: ataaGold,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                            ),
+                            underline: Container(height: 2, color: ataaGreen),
+                            onChanged: (String newValue){
+                              setState((){
+                                shirtSize = newValue;
+                              });
+                            },
+                            items: <String>['xS','S','M','L','XL','XXL','XXXL']
+                                    .map<DropdownMenuItem<String>>((String value){
+                                      return DropdownMenuItem<String>(
+                                        value: value,
+                                        child: Text(value),
+                                      );
+                                    }).toList(), 
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15,),
+                    Row(
+                      children: [
+                        Text('Shoe Size'),
+                        DropdownButton(
+                          value: shoeSize,
+                          icon: Icon(Icons.arrow_downward),
+                          iconSize: 22,
+                          style: TextStyle(
+                            color: ataaGold,
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold
+                            ),
+                            underline: Container(height: 2, color: ataaGreen),
+                            onChanged: (int newValue){
+                              setState((){
+                                shoeSize = newValue;
+                              });
+                            },
+                            items: shoesSizes.map<DropdownMenuItem<int>>((int value){
+                                      return DropdownMenuItem<int>(
+                                        value: value,
+                                        child: Text(value.toString()),
+                                      );
+                                    }).toList(),
+                            ) 
+                      ],
+                    ),
+                  ],
+                ),
+              )],
+          ));
+    });
   }
 }
